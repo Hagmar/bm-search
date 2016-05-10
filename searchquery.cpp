b7e1356574a9c6ca547389aad0205b7409d81387
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-10 18:42:36 +1000

    Generating bad character table

--- searchquery.cpp
+++ searchquery.cpp
@@ -1,5 +1,24 @@
+#include <cstring>
 #include "searchquery.h"
 
 SearchQuery::SearchQuery(char* p){
     pattern = p;
+    createBCTable();
+}
+
+void SearchQuery::createBCTable(){
+    unsigned int length = strlen(pattern);
+    for (int i = 0; i < ALPHABET_SIZE; i++){
+        bcTable[i] = new short[length];
+        memset(bcTable[i], -1, length*sizeof(unsigned short));
+    }
+
+    char c;
+    for (int i = 0; i < length-1; i++){
+        c = pattern[i];
+        for (int j = 0; j < ALPHABET_SIZE; j++){
+            bcTable[j][i+1] = bcTable[j][i];
+        }
+        bcTable[c][i+1] = i;
+    }
 }
--- searchquery.h
+++ searchquery.h
@@ -1,7 +1,14 @@
 #ifndef SEARCHQUERY_H
 #define SEARCHQUERY_H
 
+#define ALPHABET_SIZE 256
+
 class SearchQuery {
+    private:
+        short* bcTable[ALPHABET_SIZE];
+
+        void createBCTable();
+
     public:
         char* pattern;
 

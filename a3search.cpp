44b16d006a5038e031840882db92f11f1830bd0c
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-12 00:40:28 +1000

    Sort and display resulting files in correct order

--- a3search.cpp
+++ a3search.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 #include <sstream>
 #include <dirent.h>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstring>
 #include "a3search.h"
 #include "multisearch.h"
 
-void performSearch(MultiSearch* ms, char* folder, char* indexFile){
+std::vector<std::pair<float, char*> > performSearch(MultiSearch* ms,
+        char* folder, char* indexFile){
     DIR *dir;
     dirent *ent;
     std::string folderString(folder);
     folderString += "/";
+    std::vector<std::pair<float, char*> > files;
     if ((dir = opendir(folder)) != NULL){
         std::string fileNameString;
         char* fileName;
         float occurrences;
+        unsigned int length;
         while ((ent = readdir(dir)) != NULL){
             fileName = ent->d_name;
             fileNameString = std::string(fileName);
             if (fileNameString != "." && fileNameString != ".."){
                 fileNameString = folderString + fileNameString;
                 occurrences = ms->performSearch(fileNameString.c_str());
+                if (occurrences){
+                    length = strlen(fileName);
+                    char* fileNameDyn = new char[length+1];
+                    strncpy(fileNameDyn, fileName, length);
+                    fileNameDyn[length] = 0;
+                    std::pair <float,char*> pair (occurrences,fileNameDyn);
+                    files.push_back(pair);
+                }
             }
         }
         closedir(dir);
+    }
+    return files;
+}
+
+bool filesorter(std::pair<float, char*> a, std::pair<float, char*> b){
+    if (a.first > b.first){
+        return true;
+    } else if (a.first < b.first) {
+        return false;
+    } else {
+        int comparison = strcmp(a.second, b.second);
+        return comparison < 0;
+    }
+}
+
+void printResults(std::vector<std::pair<float, char*> > vector){
+    std::sort(vector.begin(), vector.end(), filesorter);
+    std::vector<std::pair<float, char*> >::iterator it;
+    for (it = vector.begin(); it != vector.end(); it++){
+        std::cout << (*it).second << " (" << (*it).first << ")" << std::endl;
     }
 }
 
@@ -40,7 +75,9 @@ int main(int argc, char** argv){
         }
         MultiSearch* searchQueries = new MultiSearch(argc-3, argv+3);
 
-        performSearch(searchQueries, folder, indexFile);
+        std::vector<std::pair<float, char*> > results;
+        results = performSearch(searchQueries, folder, indexFile);
+        printResults(results);
     }
 
     return 0;
--- a3search.h
+++ a3search.h
@@ -3,7 +3,9 @@
 
 #include "multisearch.h"
 
-void performSearch(MultiSearch*, char*, char*);
+std::vector<std::pair<float, char*> > performSearch(MultiSearch*, char*, char*);
+bool filesorter(std::pair<float, char*>, std::pair<float, char*>);
+void printResults(std::vector<std::pair<float, char* > >);
 int main(int, char**);
 
 #endif
